6063c3e47acaaae1cbb50b480b5df33b8a96ca35
Dmitry <38082886+polevoyd@users.noreply.github.com>
2018-06-07 19:54:58 -0700

    Update exercises-commented.cpp

--- exercises-commented.cpp
+++ exercises-commented.cpp
@@ -1419,3 +1419,67 @@ std::string longestDigitsPrefix(std::string inputString) {
 
 /*-------------------------------------------------------------*/
 
+int binarySearch(std::vector<int> v, int e)
+{
+	/* binary search in a vector */
+	int v_size{ (int)v.size() };
+	int left{ 0 }, right{ v_size - 1 };
+	int pivot{ v_size / 2 };
+
+	while (pivot != e)
+	{
+		if (v[pivot] == e)
+			return pivot;
+		else if (v[pivot] > e)
+		{
+			right = pivot;
+			pivot /= 2;
+		}
+		else
+		{
+			left = pivot;
+			pivot += pivot / 2;
+		}
+	}
+	return pivot;
+}
+
+/*-------------------------------------------------------------*/
+
+int digitDegree(int n) {
+
+    /* Let's define digit degree of some positive integer as the number 
+     * of times we need to replace this number with the sum of its digits 
+     * until we get to a one digit number.
+
+    Given an integer, find its digit degree.
+
+    Example
+
+    For n = 5, the output should be
+    digitDegree(n) = 0;
+    For n = 100, the output should be
+    digitDegree(n) = 1.
+    1 + 0 + 0 = 1.
+    For n = 91, the output should be
+    digitDegree(n) = 2.
+    9 + 1 = 10 -> 1 + 0 = 1. */
+    
+    int count{ 0 };
+    std::string s{ std::to_string(n) };
+    
+    while (s.size() > 1)
+    {
+        count++;
+        int temp{};
+
+        for(auto i : s)
+            temp += (i - '0');
+
+        s = std::to_string(temp);
+    }
+    
+    return count;
+}
+/*-------------------------------------------------------------*/
+
