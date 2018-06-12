40206d7fcc1385dab7dd033246477a53e271f8b7
Dmitry <38082886+polevoyd@users.noreply.github.com>
2018-06-12 08:38:38 -0700

    Update exercises-commented.cpp

--- exercises-commented.cpp
+++ exercises-commented.cpp
@@ -1834,3 +1834,37 @@ findEmailDomain(address) = "example.org". */
 }
 
 /*-------------------------------------------------------------*/
+
+std::string buildPalindrome(std::string st) {
+
+    /*
+Given a string, find the shortest possible string which 
+can be achieved by adding characters to the end of initial 
+string to make it a palindrome.
+
+Example
+
+For st = "abcdc", the output should be
+buildPalindrome(st) = "abcdcba".
+    */
+    
+    std::string ret(st);  
+    auto i = st.begin();
+    std::string reversed = std::string(ret.rbegin(), ret.rend());
+    while (ret != reversed) 
+        ret.insert(st.size(), 1, *i++);
+    return ret;
+
+}
+
+/*------------------------------------------*/
+/*		check for palindrome        */
+/*------------------------------------------*/
+
+bool isPalindrome(std::string s)
+{
+    for (int i = 0 ; i < s.size()/2 ; i++)
+        if (s[i] != s[s.size() - 1 - i])
+            return false;
+    return true;
+}
