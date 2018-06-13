c3a6cae3fddd42a0147a0b1c9e636c69bc6d9ae5
Dmitry <38082886+polevoyd@users.noreply.github.com>
2018-06-13 13:23:14 -0700

    Update exercises-commented.cpp

--- exercises-commented.cpp
+++ exercises-commented.cpp
@@ -1868,3 +1868,119 @@ bool isPalindrome(std::string s)
             return false;
     return true;
 }
+
+/*-------------------------------------------------------------*/
+
+int electionsWinners(std::vector<int> votes, int k) {
+/*
+Elections are in progress!
+
+Given an array of the numbers of votes given to each of the candidates so far, 
+and an integer k equal to the number of voters who haven't cast their vote yet, 
+find the number of candidates who still have a chance to win the election.
+
+The winner of the election must secure strictly more votes than any other candidate. 
+If two or more candidates receive the same (maximum) number of votes, assume there 
+is no winner at all.
+
+Example
+
+For votes = [2, 3, 5, 2] and k = 3, the output should be
+electionsWinners(votes, k) = 2.
+
+The first candidate got 2 votes. Even if all of the remaining 3 candidates vote for 
+him, he will still have only 5 votes, i.e. the same number as the third candidate, 
+so there will be no winner.
+The second candidate can win if all the remaining candidates vote for him (3 + 3 = 6 > 5).
+The third candidate can win even if none of the remaining candidates vote for him. 
+For example, if each of the remaining voters cast their votes for each of his opponents, 
+he will still be the winner (the votes array will thus be [3, 4, 5, 3]).
+The last candidate can't win no matter what (for the same reason as the first candidate).
+Thus, only 2 candidates can win (the second and the third), which is the answer.
+*/
+
+// if there are duplicates - then return 0
+
+
+//[1, 1, 1, 3, 3] k = 0
+
+    int max{INT_MIN};
+    int numberOfMax{0};
+    
+    for(int candidate : votes) 
+    {
+        if(candidate > max)
+            max = candidate;
+    }
+    
+    int count{0};
+    
+    for(int candidate : votes) 
+    {
+        if(candidate == max) 
+            numberOfMax++;
+        else if (candidate + k > max) 
+            count++;
+    }
+    
+    if (k == 0 && numberOfMax > 1) 
+        return 0; 
+    return numberOfMax+count;
+}
+
+/*-------------------------------------------------------------*/
+
+bool isMAC48Address(std::string inputString) {
+/*
+A media access control address (MAC address) is a unique identifier 
+assigned to network interfaces for communications on the physical 
+network segment.
+
+The standard (IEEE 802) format for printing MAC-48 addresses in 
+human-friendly form is six groups of two hexadecimal digits (0 to 9 or A to F), 
+separated by hyphens (e.g. 01-23-45-67-89-AB).
+
+Your task is to check by given string inputString whether it corresponds 
+to MAC-48 address or not.
+
+Example
+
+For inputString = "00-1B-63-84-45-E6", the output should be
+isMAC48Address(inputString) = true;
+For inputString = "Z1-1B-63-84-45-E6", the output should be
+isMAC48Address(inputString) = false;
+For inputString = "not a MAC-48 address", the output should be
+isMAC48Address(inputString) = false.
+*/
+
+std::set<int> allowed{'A', 'B', 'C', 'D', 'E', 'F', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-'};
+           
+for (int i = 0 ; i < inputString.size() ; i++)
+{
+        auto search = allowed.find(inputString[i]);
+        
+        if (search == allowed.end())
+                return false;
+        else if (((i == 2)||(i == 5)||(i == 8)||(i == 11)||(i == 14)) && (inputString[i] != '-'))
+                return false;
+        else if (inputString.size() != 17)
+                return false;
+        else if (((i != 2)&&(i != 5)&&(i != 8)&&(i != 11)&&(i != 14)) && (inputString[i] == '-'))
+                return false;
+}
+return true;
+ 
+/* regex solution    
+regex r("[0-9a-fA-F]{2}(-[0-9a-fA-F]{2}){5}");
+return regex_match(inputString,r);
+*/   
+}
+
+/*-------------------------------------------------------------*/
+
+bool isDigit(char symbol) {
+/* Determine if the given character is a digit or not. */
+return std::isdigit(symbol);
+}
+
+/*-------------------------------------------------------------*/
