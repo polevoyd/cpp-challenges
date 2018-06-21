b5f81ffcd8efa54c2b49bbddce56370366c40565
Dmitry <38082886+polevoyd@users.noreply.github.com>
2018-06-21 08:47:17 -0700

    Update exercises-commented.cpp

--- exercises-commented.cpp
+++ exercises-commented.cpp
@@ -2034,3 +2034,31 @@ std::map<char, int> makePattern(std::string subString)
     return m;
 }
 /*-------------------------------------------------------------*/
+
+int deleteDigit(int n) {
+/*
+Given some integer, find the maximal number you can obtain by 
+deleting exactly one digit of the given number.
+
+Example
+
+For n = 152, the output should be
+deleteDigit(n) = 52;
+For n = 1001, the output should be
+deleteDigit(n) = 101.
+*/
+    
+    std::string s = std::to_string(n);
+    int r{INT_MIN};
+    
+    for (int i = 0; i < s.size() ; i++)
+    {
+        std::string temp{s};
+        temp.erase(i, 1);
+        
+        if (std::stoi(temp) > r)
+            r = std::stoi(temp);
+    }
+
+    return r;
+}
