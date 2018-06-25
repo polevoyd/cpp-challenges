39e4b220e9ba14739ebbdeb7c40fb1ccc6f4011d
Dmitry <38082886+polevoyd@users.noreply.github.com>
2018-06-25 09:26:54 -0700

    Update exercises-commented.cpp

--- exercises-commented.cpp
+++ exercises-commented.cpp
@@ -2101,3 +2101,39 @@ Coud you solve it without converting the integer to a string?
     }
 };
 /*-------------------------------------------------------------*/
+
+std::string longestWord(std::string text) {
+/* 
+Define a word as a sequence of consecutive English letters. 
+Find the longest word from the given string.
+
+Example
+
+For text = "Ready, steady, go!", the output should be
+longestWord(text) = "steady".*/
+ 
+ std::string longest{""};
+ 
+ int i{ 0 };
+ 
+ while (i < text.size())
+ {
+
+    if ((96 < std::tolower(text[i])) && (std::tolower(text[i]) < 123))
+    {
+     int begin{i}, end{};
+     
+     while ((96 < std::tolower(text[i])) && (std::tolower(text[i]) < 123))
+      i++;
+     
+     std::string temp = text.substr(begin, i - begin);
+     
+     if (temp.size() > longest.size())
+      longest = temp;
+    }
+     i ++;
+ }
+
+  return longest;
+}
+/*-------------------------------------------------------------*/
