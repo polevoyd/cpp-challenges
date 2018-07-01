0c93d68dc190abd980909bc387d056bd9773a3e9
Dmitry <38082886+polevoyd@users.noreply.github.com>
2018-07-01 06:58:59 -0700

    Create Arrays: Palindrome Permutation (CTCI).cpp

---
+++ Arrays: Palindrome Permutation (CTCI).cpp
@@ -0,0 +1,8 @@
+/* Palindrome Permutation: Given a string, write a function to check if it is a permutation of a palin-
+drome. A palindrome is a word or phrase that is the same forwards and backwards. A permutation 
+is a rearrangement of letters. The palindrome does not need to be limited to just dictionary words. 
+EXAMPLE 
+Input: Tact Coa 
+Output: True (permutations: "taco cat". "atco cta". etc.) */
+
+
