54af50e88a8eda3bbcea25130a053391570a8be7
Dmitry <38082886+polevoyd@users.noreply.github.com>
2018-07-01 21:32:33 -0700

    Update Arrays: Palindrome Permutation (CTCI).cpp

--- Arrays: Palindrome Permutation (CTCI).cpp
+++ Arrays: Palindrome Permutation (CTCI).cpp
@@ -17,6 +17,35 @@ bool palindromeRearranging(std::string inputString) {
 
      * We can rearrange "aabb" to make "abba", which is a palindrome. */
     
+     // USING SET
+     // TIME: N LINEAR - still going once through string
+     // SPACE: N LINEAR - cause creating a set
+     // creating a set of unique characters to count how many doesn't have a pair
+     // while adding them to set we check if they already have a pair in a set
+     // if one is already there - delete them both
+     // if no - adding it cause its unique one (so far)
+     // at end we have a set of unique chars that doesnt have a pair
+     // if there is more than one char - it cannot be a palindrome
+     std::set<char> unique_characters{};
+     
+     for (auto i : inputString)
+     {
+          // if one already there - delete it
+          // if not - add
+          if (unique_characters.find(i) != unique_characters.end())
+          {
+               unique_characters.erase(i);
+          }
+          else
+          {
+               unique_characters.insert(i);
+          }
+          
+     }
+     
+     return unique_characters.size() <= 1 ? true : false;
+     
+     // USING HASH TABLE:
      // TIME: N + N = N LINEAR( two times run through string to create map and to count chars)
      // SPACE: N - cause we creating a map
      // Basically, the task is to know if that word can be possibly a palindrome if
