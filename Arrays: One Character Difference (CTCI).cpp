79bf63f1c3ec6e45c61bf68829f661bc38dede7a
Dmitry <38082886+polevoyd@users.noreply.github.com>
2018-07-03 12:57:06 -0700

    Update Arrays: One Character Difference (CTCI).cpp

--- Arrays: One Character Difference (CTCI).cpp
+++ Arrays: One Character Difference (CTCI).cpp
@@ -9,3 +9,35 @@ pale. bale -> true
 pale. bake -> false
 */
 
+bool isOneAway(std::string s1, std::string s2){
+	
+  // a- longest string, b - shortest
+  std::string a,b;
+	a = s1.size() >= s2.size() ? s1 : s2;
+	b = s1.size() < s2.size() ? s1 : s2;
+  
+  // if difference of strings more than 1 char its false
+  if (std::abs(a.size() - b.size()) > 1)
+		return false;
+  
+	bool flag = false;
+	
+  for(int i = 0, j = 0; i < a.size() && j < b.size(); )
+  {
+		if( a[i] != b[j])
+    {
+			if(flag)
+				return false;
+      
+			flag = true;
+			
+      if(a.size() == b.size())
+				i++,j++;
+			else
+				i++;
+		}
+		else
+			i++,j++;
+	}
+	return true;
+}
